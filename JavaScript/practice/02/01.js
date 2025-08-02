80a0d374f1ce272f7f2c0cb53298d62c2e9f0557
dongha <donghak937@gmail.com>
2025-08-03 00:18:38 +0900

    add

--- JavaScript/practice/02/01.js
+++ JavaScript/practice/02/01.js
@@ -7,14 +7,3 @@ function myFunction(){
 
 myFunction();
 console.log(name);
-
-
-let name2 = 'Bormi';
-
-function myFunction2(){
-    let name2 = 'Luge';
-    console.log('지역 변수 : ' + name2);
-}
-
-myFunction2();
-console.log('전역 변수 : ' + name2);
