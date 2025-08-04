380263e841c40600156be8b3bb43ef5f0ae7874b
dongha <donghak937@gmail.com>
2025-08-04 15:02:48 +0900

    add

--- JavaScript/practice/05/01.js
+++ JavaScript/practice/05/01.js
@@ -1,5 +1,15 @@
 let a = 10;
 let b = 20;
 
-if ( a % b == 0) console.log('a는 b의 배수입니다.');
-else console.log('a는 b의 배수가 아닙니다.');
+function check(a ,b){
+
+    if (a <= 0 || b <= 0){
+        console.log('error');
+        return;
+    }
+
+    if ( a % b == 0) console.log('a는 b의 배수입니다.');
+    else console.log('a는 b의 배수가 아닙니다.');
+}
+
+check(a,b);
