a72b7e606b75bd98c1242fc0e50ce655cd4069a8
dongha <donghak937@gmail.com>
2025-08-05 11:49:37 +0900

    add

---
+++ homework/Practice01.js
@@ -0,0 +1,20 @@
+let name1 = 'dongha';
+let age1 = '31';
+let hobby1 = 'book';
+let gender1 = 'man';
+
+function practice(){
+
+    let name2 = 'junha';
+    let age2 = '12';
+    let hobby2 = 'shit';
+    let gender2 = 'man';
+
+    console.log('이름 비교 결과 ' + name1 + ' ' + name2 + ' 결과 : ' + (name1 == name2));
+    console.log('나이 비교 결과 ' + age1 + ' ' + age2 + ' 결과 : ' +  (age1 == age2));
+    console.log('취미 비교 결과 ' + hobby1 + ' ' + hobby2 + ' 결과 : ' +  (hobby1 == hobby2));
+    console.log('성별 비교 결과 ' + gender1 + ' ' + gender2 + ' 결과 : ' +  (gender1 == gender2));
+}
+
+practice();
+
